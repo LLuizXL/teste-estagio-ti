03d259577d5280c3f0ac343c94a3c7a0497c587b
LuizG <luiz.mwlo76@gmail.com>
2026-07-06 18:37:53 -0300

    Adiciona react-icons no projeto e atualiza componente Knowledges para utilizar ícones nas habilidades

--- src/pages/Conhecimentos.jsx
+++ src/pages/Conhecimentos.jsx
@@ -1,3 +1,4 @@
+import { BsDatabase } from "react-icons/bs";
 import Knowledges from "../components/Knowledges";
 
 export const Conhecimentos = () => {
@@ -5,13 +6,7 @@ export const Conhecimentos = () => {
     <main className="container d-flex flex-column gap-5">
       <h2 className="">Minhas Habilidades e Especialidades: </h2>
       <section className="d-flex flex-wrap gap-5 justify-content-center">
-        <Knowledges nome={"nome 2"} descricao={"descricao da habilidade"} />
-        <Knowledges nome={"nome 2"} descricao={"descricao da habilidade"} />
-        <Knowledges nome={"nome 2"} descricao={"descricao da habilidade"} />
-        <Knowledges nome={"nome 2"} descricao={"descricao da habilidade"} />
-        <Knowledges nome={"nome 2"} descricao={"descricao da habilidade"} />
-        <Knowledges nome={"nome 2"} descricao={"descricao da habilidade"} />
-        <Knowledges nome={"nome 2"} descricao={"descricao da habilidade"} />  
+        <Knowledges nome={"nome 2"} descricao={"descricao da habilidade"} Icon={BsDatabase} />
       </section>
     </main>
   );
